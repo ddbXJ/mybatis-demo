fec1c38981668d38e6ad1bf4d938bf70f01c4ff8
lee xj <lxj9207@gmail.com>
2016-07-17 20:42:26 +0800

    complete annotation test

--- src/main/java/com/lxj/mapping/UserMapperI.java
+++ src/main/java/com/lxj/mapping/UserMapperI.java
@@ -1,7 +1,12 @@
 package com.lxj.mapping;
 
 import com.lxj.domain.User;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
 
 /**
  * Created by lxj on 2016/7/15.
@@ -10,4 +15,16 @@ public interface UserMapperI {
 
     @Insert("insert into users(name,age) values (#{name},#{age})")
     public int add(User user);
+
+    @Select("select * from users where id=#{id}")
+    public User selectById(int id);
+
+    @Update("update users set name=#{name},age=#{age} where id=#{id}")
+    public int update(User user);
+
+    @Delete("delete from users where id=#{id}")
+    public int delete(int id);
+
+    @Select("select * from users")
+    public List<User> selectAll();
 }
--- src/test/java/TestCRUDByAnnotationMapper.java
+++ src/test/java/TestCRUDByAnnotationMapper.java
@@ -4,6 +4,8 @@ import com.lxj.util.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 /**
  * Created by lxj on 2016/7/15.
  */
@@ -17,5 +19,43 @@ public class TestCRUDByAnnotationMapper {
         int i = userMapperI.add(user);
         sqlSession.close();
         System.out.println(i);
+    }
+
+    @Test
+    public void testSelect() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSession(true);
+        UserMapperI userMapperI = sqlSession.getMapper(UserMapperI.class);
+        User user = userMapperI.selectById(1);
+        sqlSession.close();
+        System.out.println(user);
+    }
+
+    @Test
+    public void testUpdate() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSession(true);
+        UserMapperI userMapperI = sqlSession.getMapper(UserMapperI.class);
+        User user = new User(10,"ydd",15);
+        int i = userMapperI.update(user);
+        sqlSession.close();
+        System.out.print(i);
+    }
+
+    @Test
+    public void testDelete() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSession(true);
+        UserMapperI userMapperI = sqlSession.getMapper(UserMapperI.class);
+        int i = userMapperI.delete(11);
+        System.out.println(i);
+    }
+
+    @Test
+    public void selectAll() {
+        SqlSession sqlSession = MyBatisUtil.getSqlSession(true);
+        UserMapperI userMapperI = sqlSession.getMapper(UserMapperI.class);
+        List<User> users = userMapperI.selectAll();
+        for (User u : users) {
+            System.out.println(u);
+        }
+        sqlSession.close();
     }
 }
